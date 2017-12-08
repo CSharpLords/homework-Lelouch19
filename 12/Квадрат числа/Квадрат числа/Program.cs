ed2b36eb9ee5cd61a0547569faec50db67af5d56
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-08 16:40:11 +0300

    Квадрат числа

---
+++ 12/Квадрат числа/Квадрат числа/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Поиск_квадратов
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (double n = 0.1; n <= 0.9; n = n + 0.1)
+            {
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            Console.ReadLine();
+        }
+    }
+}
