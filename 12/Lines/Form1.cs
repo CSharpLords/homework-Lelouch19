3219428ab254fe15c627d0ca27dd05e0ba324b17
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-08 16:45:50 +0300

    Ручки (FOR)

--- 12/Lines/Form1.cs
+++ 12/Lines/Form1.cs
@@ -20,14 +20,10 @@ namespace LoopyLandscape {
 			graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
 			Pen pen = new Pen(Brushes.BlueViolet, 8);
-			// Упростите код с помощью цикла for
-			graphics.DrawLine(pen, 20, 40, 80, 80); // первая пара чисел - координаты начала отрезка, вторая - координаты конца
-			graphics.DrawLine(pen, 80, 40, 140, 80);
-			graphics.DrawLine(pen, 140, 40, 200, 80);
-			graphics.DrawLine(pen, 200, 40, 260, 80);
-			graphics.DrawLine(pen, 260, 40, 320, 80);
-			graphics.DrawLine(pen, 320, 40, 380, 80);
-			graphics.DrawLine(pen, 380, 40, 440, 80);
+            for(int n = 20; n <= 380; n = n + 60)
+            {
+                graphics.DrawLine(pen, n, 40, n + 60, 80);
+            }
 		}
 	}
 }
