a38229b0950dcc7c90f0353a103c2383e410cedd
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-09 12:15:32 +0300

    Слова наоборот

---
+++ 12/Слова наоборот/Слова наоборот/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Слова_наоборот
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите слово.");
+            string Word = Console.ReadLine().ToLower();
+
+            for(int letter = 0; letter < Word.Length; letter = letter + 1)
+            {
+                int End = Word.Length - 1;
+                Console.WriteLine(Word[End - letter]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
