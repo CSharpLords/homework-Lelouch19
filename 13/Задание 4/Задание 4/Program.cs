0bb7a12991b9d204c90d023e6d15543e1638beb3
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-10 17:00:23 +0300

    Задание 4

---
+++ 13/Задание 4/Задание 4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите загаданное слово.");
+            string Word = Console.ReadLine().ToLower();
+            int Letter = 0;
+            int End = Word.Length - 1;
+
+            for (int attempt = 0; attempt <= 9; attempt = attempt + 1)
+            {
+                Console.WriteLine("Введите букву.");
+                char Letter2 = char.Parse(Console.ReadLine());
+
+                if (Letter2 == Word[Letter])
+                {
+                    Console.WriteLine("Вы угадали букву!");
+                    Console.WriteLine();
+                    Letter = Letter + 1;
+
+                    if (Letter2 == Word[Word.Length - 1])
+                    {
+                        attempt = 10;
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("Вы не угадали букву.");
+                    
+                }
+              
+
+            }
+            Console.WriteLine("Вы угадали слово!");
+            Console.ReadLine();
+
+        }
+    }
+}
