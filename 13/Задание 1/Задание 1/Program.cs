136b5fa7b036b8cdd70abe51893bd5e894670b08
Daniel <32432727+Lelouch19@users.noreply.github.com>
2017-12-10 15:40:39 +0300

    Задание1

---
+++ 13/Задание 1/Задание 1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("У тебя есть компьютер?");
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "нет") 
+            {
+                Console.WriteLine("Ну ок, тогда нам не о чем говорить");
+                Console.ReadLine();
+                Environment.Exit(0);
+            }
+            else if (answer == "да")
+            {
+                Console.WriteLine("Сколько часов в неделю ты проводишь за ним?");            
+            }
+
+            int answer2 = int.Parse(Console.ReadLine());
+
+            if (answer2 < 14)
+            {
+
+                Console.WriteLine("А большую часть этого времени ты работаешь или отдыхаешь?");
+                string answer4 = Console.ReadLine().ToLower();
+
+                if (answer4 == "работаю")
+                {
+                    Console.WriteLine("Ты и отдохнуть не забывай");
+                    Console.ReadLine();
+                }
+                else if (answer4 == "отдыхаю")
+                {
+                    Console.WriteLine("Хорошо, что ты знаешь меру");
+                    Console.ReadLine();
+                }
+               
+            }
+
+
+            else if (answer2 >= 14)
+            {
+                Console.WriteLine("А большую часть этого времени ты работаешь или отдыхаешь?");
+                
+                string answer3 = Console.ReadLine().ToLower();
+
+                if (answer3 == "работаю")
+                {
+                    Console.WriteLine("Пожалей себя!!");
+                    Console.ReadLine();
+                }
+                else if (answer3 == "отдыхаю")
+                {
+                    Console.WriteLine("Хватит портить своё здоровье!");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+    
+    }
+}
