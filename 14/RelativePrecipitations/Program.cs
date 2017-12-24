285891104dde76211ab9aefccabc0717ccc02ac2
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 12:57:17 +0300

    Как-то так

--- 14/RelativePrecipitations/Program.cs
+++ 14/RelativePrecipitations/Program.cs
@@ -6,12 +6,15 @@ using System.Threading.Tasks;
 
 namespace RelativePrecipitations {
 	class Program {
-		static void Main(string[] args) {
+		static void Main(string[] args)
+        {
+            int changes = 0;
 			int[] precipitations = { 23, 4545, 45, 2, -16, 765, 341, 1, -3, 0 };
 
-			for (int i = 0; i < precipitations.Length; i++) {
-				precipitations[i] = precipitations[i] - precipitations[0];
-				Console.WriteLine(precipitations[i]);
+			for (int i = 0; i < precipitations.Length; i++)
+            {
+				changes = precipitations[i] - precipitations[0];
+				Console.WriteLine(changes);
 			}
 			Console.ReadLine();
 		}
