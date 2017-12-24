c4104e0f0e230ef592ae32be41b18ec050dc9dfb
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 12:25:19 +0300

    Проверка элементов массива

---
+++ 14/Проверка элемента массива/Проверка элемента массива/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_элемента_массива
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Massif = { 1, 5, 89, -67, 0 };
+            for(int n = 0; n < Massif.Length; n = n + 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Введите номер массива.В массиве всего 5 чисел.");
+                int a = int.Parse(Console.ReadLine());
+                if (Massif[a] > 0)
+                {
+                    Console.WriteLine("Число положительное!");
+                }
+                else if (Massif[a] == 0)
+                {
+                    Console.WriteLine("Число равно нулю!");
+                }
+                else
+                {
+                    Console.WriteLine("Число отрицательное!");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 14/Проверка элемента массива/Проверка элемента массива/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Проверка элемента массива")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Проверка элемента массива")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("1f65f18b-d4e2-46cb-b018-1c08c4d1b2d9")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
