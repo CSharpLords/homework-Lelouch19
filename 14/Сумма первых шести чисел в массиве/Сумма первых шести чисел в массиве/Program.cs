66182676246d76ea11f4132340d6063eb78c50d8
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 12:51:14 +0300

    Сумма первых 6 чисел в массиве

---
+++ 14/Сумма первых шести чисел в массиве/Сумма первых шести чисел в массиве/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Сумма_первых_шести_чисел_в_массиве
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] Massif = { 1, 567, 785, 896, -98, 0, 987, 456 };
+            for(int n = 0; n < 6; n  = n + 1)
+            {
+                sum = sum + Massif[n];
+            }
+            Console.WriteLine("Сумма - " + sum);
+            Console.ReadLine();
+        }
+    }
+}
