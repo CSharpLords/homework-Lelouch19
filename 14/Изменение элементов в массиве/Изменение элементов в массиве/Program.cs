b80f8dcc3157dc356be70d69c5c5fbea5297d7b0
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 12:32:22 +0300

    Изменение элементов в массиве

---
+++ 14/Изменение элементов в массиве/Изменение элементов в массиве/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Изменение_элементов_в_массиве
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Massif = { 1, 56, 89, -10, 97 };
+            while (true)
+            {
+                Console.WriteLine("Введите номер элемента.");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите число,на которое хотите заменить число в массиве.");
+                int a = int.Parse(Console.ReadLine());
+                Massif[n] = a;
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
