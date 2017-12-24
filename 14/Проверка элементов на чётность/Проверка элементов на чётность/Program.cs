10e60c43b0d387995a428f0c903d9cc16ebe7967
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 12:46:34 +0300

    Является ли сумма элементов массива четным числом?

---
+++ 14/Проверка элементов на чётность/Проверка элементов на чётность/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Проверка_элементов_на_чётность
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int[] Massif = {1, 78, 89, -156, 0};
+            for(int n = 0; n < Massif.Length; n = n + 1)
+            {
+               sum = sum + Massif[n];
+            }
+            int Rest = sum % 2;
+            if(Rest == 0)
+            {
+                Console.WriteLine("Сумма элементов в массиве является чётным числом!");
+            }
+            else
+            {
+                Console.WriteLine("Сумма элементов в массиве является нечётным числом!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
