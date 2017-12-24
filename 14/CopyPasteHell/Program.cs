9c57208bbb387f36b8f12d62e762d8ffec8187bb
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2017-12-24 12:37:30 +0300

    Сократил код с помощью for

--- 14/CopyPasteHell/Program.cs
+++ 14/CopyPasteHell/Program.cs
@@ -8,16 +8,12 @@ namespace CopyPasteHell {
 	class Program {
 		static void Main(string[] args) {
 			float[] numbers = { 765, 341, 1, -3, 0, 13, -200, 23, 44 };
-
-			numbers[0] = numbers[0] / 5;
-			numbers[1] = numbers[1] / 5;
-			numbers[2] = numbers[2] / 5;
-			numbers[3] = numbers[3] / 5;
-			numbers[4] = numbers[4] / 5;
-			numbers[5] = numbers[5] / 5;
-			numbers[6] = numbers[6] / 5;
-			numbers[7] = numbers[7] / 5;
-			numbers[8] = numbers[8] / 5;
+            for(int n = 0; n < numbers.Length; n = n + 1)
+            {
+                numbers[n] = numbers[n] / 5;
+                Console.WriteLine(numbers[n]);
+            }
+			
 			Console.ReadLine();
 		}
 	}
