dbf4145e09d14631e5e0f4b1c2e2463ebaa15c14
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-01-06 18:35:55 +0300

    Задание с лягушками

--- 15/PlaceFrogs/Form1.cs
+++ 15/PlaceFrogs/Form1.cs
@@ -7,28 +7,31 @@ namespace PlaceFrogs {
         Image frog = Image.FromFile("../../frog.png");
         Image lilypad = Image.FromFile("../../lilypad.png");
 
-        public Form1() {
+        public Form1()
+        {
             InitializeComponent();
         }
 
-        protected override void OnPaint(PaintEventArgs e) {
+        protected override void OnPaint(PaintEventArgs e)
+        {
             graphics = e.Graphics;
             graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
 
             // рисуем кувшинки и лягушек
-            for (int i = 0; i < 1000; i += 250) {
+            for (int i = 0; i < 1000; i += 250)
+            {
                 DrawLilypad(i, 0);
-            }
-            for (int i = 50; i < 1000; i += 250) {
-                DrawFrog(i, 35);
+                DrawFrog(i + 50, 35);
             }
         }
 
-        private void DrawFrog(int x, int y) {
+        private void DrawFrog(int x, int y)
+        {
             graphics.DrawImage(frog, x, y, 150, 150);
         }
 
-        private void DrawLilypad(int x, int y) {
+        private void DrawLilypad(int x, int y)
+        {
             graphics.DrawImage(lilypad, x, y, 250, 250);
         }
     }
