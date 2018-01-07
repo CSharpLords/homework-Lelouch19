f0c4100070958bc2be1ab956e816ac2eccce22c2
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-01-07 16:55:39 +0300

    Вывод элементов массива

---
+++ 15/Вывод элементов массива/Вывод элементов массива/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Вывод_элементов_массива
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = {245, 8, 1987, -56, 6789, 0};
+            for(int n = 1; n < Numbers.Length; n = n + 1)                 //Элемент с индексом "0" не выводим,поэтому начинаем с элемента с индексом "1"
+            {
+                if(Numbers[n] > Numbers[n - 1])
+                {
+                    Console.WriteLine(Numbers[n]);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
