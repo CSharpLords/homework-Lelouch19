8ff8514ea62a9332d1ea5ad9cd1cc4cfadfd7790
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-01-08 16:07:22 +0300

    Массив с рандомным ростом

---
+++ 15/Массив с рандомным ростом/Массив с рандомным ростом/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Массив_с_рандомным_ростом
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int[] growth = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ,0};
+            for(int n = 0; n < growth.Length; n = n + 1)
+            {
+                int r = rand.Next(163, 190);
+                growth[n] = r;
+                Console.WriteLine(growth[n]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
