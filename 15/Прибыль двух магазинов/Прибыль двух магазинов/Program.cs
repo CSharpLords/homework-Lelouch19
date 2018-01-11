42b8c0437e41b4cf22c66c9f7df831ffcf359380
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-01-11 19:50:08 +0300

    Переделал задачу про магазины

--- 15/Прибыль двух магазинов/Прибыль двух магазинов/Program.cs
+++ 15/Прибыль двух магазинов/Прибыль двух магазинов/Program.cs
@@ -15,25 +15,18 @@ namespace Прибыль_двух_магазинов
             int sum = 0;
 
             Random rand = new Random();
-            int[] FirstShop = { 0 };
+            int[] FirstShop = new int [61];
             for(int n = 0; n < FirstShop.Length; n = n + 1)
-            {
-                for(int day = 0; day <= 61; day = day + 1)
-                {
+            {             
                     int r = rand.Next(0, 10000);
-                    sum1 = sum1 + r;
-                }
-               
+                    sum1 = sum1 + r;            
             }
 
-            int[] SecondShop = { 0 };
+            int[] SecondShop = new int[61];
             for(int y = 0; y < SecondShop.Length; y = y + 1)
-            {
-                for (int days = 0; days <= 61; days = days + 1)
-                {
+            {            
                     int R = rand.Next(0, 100000);
                     sum2 = sum2 + R;
-                }
             }
 
             sum = sum1 + sum2;
