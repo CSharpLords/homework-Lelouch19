99f1b5a8137dc19bb62a1f6fdf014a37366758ba
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-01-12 15:58:56 +0300

    Исправил(Но в правельности решения все же не уверен)

--- 15/Числа кратные 3/Числа кратные 3/Program.cs
+++ 15/Числа кратные 3/Числа кратные 3/Program.cs
@@ -10,19 +10,16 @@ namespace Числа_кратные_3
     {
         static void Main(string[] args)
         {
-            int x = 1;
-            int[] Numbers = new int[x];
+            int[] Numbers = new int[1];
             for(int n = 0; n < Numbers.Length; n = n + 1)
             {
                 for(int y = 60; y >= 10;y = y - 1)
                 {
-
                     int Remainder = y % 3;
                     if (Remainder == 0)
                     {
                         Numbers[n] = y;
                         Console.WriteLine(Numbers[n]);
-                        x = x + 1;
                     }
 
                 }
