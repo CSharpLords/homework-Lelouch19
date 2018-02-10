13ff15e00754230fc3afcf00e856bfdeda10cb80
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-02-10 17:12:08 +0300

    Секундомер

---
+++ 18/Секундомер/Assets/Scripts/Go.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Go : MonoBehaviour {
+	
+	float time = 0;
+	void Start()
+	{
+		print ("Начинаем!");
+	}
+	void Update () 
+	{
+		transform.Rotate(new Vector3(0, 6, 0) * Time.deltaTime);
+
+		time = time + Time.deltaTime;
+		float seconds = (int)time;
+		float r = seconds % 10;
+		if (r == 0) 
+		{
+			print ("Прошло - " + seconds + " секунд.");
+		}
+
+	}
+}
