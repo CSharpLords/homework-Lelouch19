69433ca35d349f9840f27fb449dc5ca613cd297d
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-02-12 18:47:37 +0300

    Домашка за 17-ый урок

---
+++ 17/HomeWork 17/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+	
+	public Transform player;
+	float time = 20;
+	void Start () 
+	{
+		print ("Найди врага!");
+	}
+
+	void Update () 
+	{
+		time = time - Time.deltaTime;
+		float seconds = (int)time;
+		print (seconds);
+		float dist = Vector3.Distance(player.position,transform.position);
+		if (dist <= 5) 
+		{
+			print ("Вы нашли его!");
+		}
+		if(seconds == 0)
+		{
+			print ("Время вышло!");
+			time = 20;
+		}
+	}
+}
