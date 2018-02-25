476a83ddfbf1de825b37b0d74677575e34348426
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-02-25 12:54:25 +0300

    Охраник(Не доделал)

---
+++ 20/HomeWork 20.Задание 2/Assets/Scripts/SecurityGuard.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecurityGuard : MonoBehaviour 
+{
+	public Transform[] objects; 
+
+	void Start () 
+	{
+		
+	}
+
+	void Update () 
+	{
+		for (int a = 0; a < objects.Length; a++) 
+		{
+			Vector3 direction  = objects[a].position - transform.position;
+			direction.Normalize ();
+			transform.Translate (direction * 5 * Time.deltaTime);
+		}
+	}
+}
