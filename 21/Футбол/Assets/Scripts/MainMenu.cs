ec9cb0dccf65c4b8a0ae6d5a577100a56b20f87b
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-03-03 20:55:08 +0300

    Футбол(Кнопки не работают)

---
+++ 21/Футбол/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+	public Button startButton;
+	public Button creditsButton;
+	public Button exitButton;
+
+	void Start () 
+	{
+		Button startButton = GetComponent<Button> (); 
+		Button creditsButton = GetComponent<Button> (); 
+		Button exitButton = GetComponent<Button> (); 
+
+		startButton.onClick.AddListener(LoadGame);
+		exitButton.onClick.AddListener(ExitGame);
+	}
+	void LoadGame() 
+	{
+		SceneManager.LoadScene("Field");
+	}
+	void ExitGame() 
+	{
+		Application.Quit();
+	}
+}
