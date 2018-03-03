d9188d5b4b29ede836123344418da07ef27f2dfa
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-03-03 21:48:53 +0300

    Банковский счёт
    Старый добрый Visual Studio)

---
+++ 21/Банковский счёт/Банковский счёт/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Банковский_счёт
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Money = { 56700, 8999, 76, 1090, -145000, 9000, 567, 890, 6578, 9 };
+            int x = 0;
+            for (int n = 0; n < Money.Length;n ++)
+            {
+                if (Money[n] < 0)
+                {
+                    x = x + 1;
+                    break;
+                }
+      
+            }
+            if (x == 1)
+            {
+                Console.WriteLine("Задолжники есть!");
+            }
+            else
+            {
+                Console.WriteLine("Задолжников нет!");
+            }
+            Console.ReadLine();
+        }
+    }
+}
