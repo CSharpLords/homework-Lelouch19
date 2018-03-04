a4e6bbc07f5dd2413b1c558ddd70b4ab21e1a44d
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-03-04 16:50:59 +0300

    Кликер

---
+++ 21/Кликер/Assets/Scripts/Clicker.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Clicker : MonoBehaviour {
+	
+	public Button clickButton;
+	public Text scoreField;
+	public Text timeField;
+	public float time = 30;
+	public int click = 0;
+
+	void Start () 
+	{
+		clickButton.onClick.AddListener(Score);
+	}
+	void Update()
+	{
+		time = time - Time.deltaTime;
+		timeField.text = ((int)time).ToString();
+		if(time < 0 )
+		{
+			End ();
+		}
+	}
+	void Score()
+	{
+		click = click + 1; 
+		scoreField.text = click.ToString();
+	}
+	void End()
+	{
+		SceneManager.LoadScene("End");
+	}
+
+}
