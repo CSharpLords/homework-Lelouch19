010a786deb23fc51e42c382452886664b0a81ff0
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-03-09 22:58:31 +0300

    Меняем цвет машины

---
+++ 22/Меняем цвет машины/Assets/Scripts/Colours.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Colours : MonoBehaviour {
+
+	public Button GreenButton;
+	public Button BlueButton;
+	public Button RedButton;
+	public Renderer rend;
+
+	void Start () 
+	{
+		GreenButton.onClick.AddListener(Green);
+		BlueButton.onClick.AddListener(Blue);
+		RedButton.onClick.AddListener(Red);
+	}
+	void Red () 
+	{
+		rend.material.color = Color.red;
+	}
+	void Green () 
+	{
+		rend.material.color = Color.green;
+	}
+	void Blue () 
+	{
+		rend.material.color = Color.blue;
+	}
+}
