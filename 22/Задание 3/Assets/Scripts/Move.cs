158ffb7ed51a6d64cdb2be5591177b14d5713a9a
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-03-09 22:22:21 +0300

    Задание 3

---
+++ 22/Задание 3/Assets/Scripts/Move.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour 
+{
+	public Transform[] cubs;
+	float a = 0;
+
+	void Update () 
+	{
+		for(int x = 0; x < cubs.Length; x ++) //Передвижение объектов
+		{
+			Vector3 direction  = transform.position - cubs[x].position;
+			direction.Normalize ();
+			cubs[x].Translate(direction * 5 * Time.deltaTime);
+		}
+
+		for(int y = 0; y < cubs.Length; y ++) //Проверка расстояния
+		{ 
+			float dist = Vector3.Distance(cubs[y].position, transform.position);
+			if (dist < 2) 
+			{
+				a = 1;
+			} 
+			else 
+			{
+				a = 0;
+			}
+		}
+
+		if (a == 1) 
+		{
+			print ("Все на месте!");
+		}
+	}
+}
