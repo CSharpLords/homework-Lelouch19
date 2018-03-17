6784b648400d049920d99fbf118122b721c7aaeb
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-03-17 15:36:50 +0300

    19/01

--- 19/HomeWork 19/Assets/Scripts/Colour.cs
+++ 19/HomeWork 19/Assets/Scripts/Colour.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 
 public class Colour : MonoBehaviour {
 	
-	Renderer colour;
-	float a = 0;
+	Renderer rend;
 	float time = 0;
 
 	void Start () 
 	{
-		colour = GetComponent<Renderer>();
-		//colour.material.color =  Color.red;
+		rend = GetComponent<Renderer>();
 	}
-
+		
 	void Update () 
 	{
-		time = time + Time.deltaTime;
-		int seconds = (int)time;
-		int r = seconds % 2;
-		if (r == 0) 
-		{
-			colour.material.color = new Color (255, a + 1, a + 1);
-			if (a == 255) 
-			{
-				a = 0;
-			}
-		}
 
+		if (time > 5)
+		{
+			time = 0;
+			time = time + Time.deltaTime;
+			rend.material.color = Color.Lerp(Color.white, Color.red, time / 5);
+		}
+		time = time + Time.deltaTime;
+		rend.material.color = Color.Lerp(Color.red, Color.white, time / 5);
 	}
 
 }
