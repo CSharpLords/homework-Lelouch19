d66a8216818f8bb6baded9986999781e39751a0b
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-08 15:01:03 +0300

    Hello

--- 26/Hello/Program.cs
+++ 26/Hello/Program.cs
@@ -5,8 +5,11 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Hello {
-	class Program {
-		static void Main(string[] args) {
+	class Program 
+    {
+        static Random numbers = new Random();
+		static void Main(string[] args) 
+        {
 			SayHello("Маша");
 			SayHello("Женя");
 			SayHello("Петя");
@@ -14,8 +17,21 @@ namespace Hello {
 			Console.ReadLine();
 		}
 
-		static void SayHello(string name) {
-			// пиши код здесь
+		static void SayHello(string name) 
+        {
+            int privet = numbers.Next(0, 3);
+            if (privet == 0)
+            {
+                Console.WriteLine("Добрый день, " + name);
+            }
+            if (privet == 1)
+            {
+                Console.WriteLine("Здравствуй, " + name);
+            }
+            if (privet == 2)
+            {
+                Console.WriteLine("Привет, " + name);
+            }
 		}
 	}
 }
