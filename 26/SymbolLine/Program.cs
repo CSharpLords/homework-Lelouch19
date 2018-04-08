4058dd3fe22637c8ade8b43c6d852d77baccc180
Daniel <32432727+Lelouch19@users.noreply.github.com>
2018-04-08 15:16:15 +0300

    SymbolLine

--- 26/SymbolLine/Program.cs
+++ 26/SymbolLine/Program.cs
@@ -4,13 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SymbolLine {
-	class Program {
-		static void Main(string[] args) {
+namespace SymbolLine 
+{
+	class Program 
+    {
+		static void Main(string[] args)
+        {
 			PrintSymbols("*", 10);
 			PrintSymbols("+", 5);
 
 			Console.ReadLine();
 		}
+        static void PrintSymbols(string symbol,int dlina)
+        {
+            string symbols = "";
+            for (int x = 0; x < dlina; x++)
+            {
+                symbols = symbols + symbol;
+            }
+            Console.WriteLine(symbols);
+        }
 	}
 }
