f5399676bf4ca87a0886f623f7b35299d04445a8
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-05-11 18:33:43 +0300

    SpititualWolf

--- 31/SpiritualWolf/Program.cs
+++ 31/SpiritualWolf/Program.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SpiritualWolf {
-	class Program {
-		static void Main(string[] args) {
+namespace SpiritualWolf
+{
+	class Program
+    {
+		static void Main(string[] args)
+        {
 			Wolf wolf = new Wolf(new string[] { "Омммм", "Поклонение восьмирукому и трёхглазому Победителю Смерти, сидящему в падмасане", "ОМ! О, Земля, Небо, Сварга!" });
 			wolf.Sing();
 			wolf.Sing();
