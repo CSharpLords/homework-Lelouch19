5371c4a6a23d29e1d25ad1468bc05be6b9d0caaa
Lelouch19 <32432727+Lelouch19@users.noreply.github.com>
2018-05-12 14:56:11 +0300

    CatFactory

--- 31/CatFactory/Cat.cs
+++ 31/CatFactory/Cat.cs
@@ -1,8 +1,51 @@
-﻿namespace CatFactory {
-	class Cat {
+﻿namespace CatFactory
+{
+	class Cat
+    {
 		public string name;
 		public double age;
 		public double weight;
 		public double strenght;
+        int score = 0;
+        public Cat(string name, double age, double weight, double strenght)
+        {
+            this.name = name;
+            this.age = age;
+            this.weight = weight;
+            this.strenght = strenght;
+        }
+        public string Fight(Cat kot)
+        {
+            if (age > kot.age)
+            {
+                score++;
+                if (weight > kot.weight)
+                {
+                    score++;
+                    if (strenght > kot.strenght)
+                    {
+                        score++;
+                    }
+                }
+            }
+            if (age < kot.age)
+            {
+                kot.score++;
+                if (weight < kot.weight)
+                {
+                    kot.score++;
+                    if (strenght < kot.strenght)
+                    {
+                        kot.score++;
+                    }
+                }
+            }
+
+            if(score < kot.score)
+            {
+                 kot.name = name;
+            }
+            return name;
+        }
 	}
 }
--- 31/CatFactory/Program.cs
+++ 31/CatFactory/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 
-namespace CatFactory {
-	class Program {
-		static void Main(string[] args) {
+namespace CatFactory
+{
+	class Program
+    {
+		static void Main(string[] args)
+        {
 			Cat barsik = new Cat("Барсик", 3, 2.5, 10);
 			Cat kuzya = new Cat("Кузя", 2.5, 4, 5);
 			Cat ryzhik = new Cat("Рыжик", 5, 3, 6);
